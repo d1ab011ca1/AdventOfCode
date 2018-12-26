5c2af5a019f78aa706a0d32a92b8cab6a87c3ed4
John Belcher <belcherjohn@gmail.com>
2018-12-25 23:15:48 -0500

    2018 Day 22  part 2 - Failure! Sol'n yields wrong answer.

--- 2018/Day22.ps1
+++ 2018/Day22.ps1
@@ -7,7 +7,7 @@ Clear-Host
 
 function main {
     #part1 #= 5637
-    part2 #= 
+    part2 #= 965 (wrong answer!!!)
 }
 
 class Row : System.Collections.ArrayList {
@@ -204,22 +204,186 @@ function part1 {
     Write-Host "Part 1: Risk is $risk."
 }    
 
+Add-Type -TypeDefinition '
+public class Node : System.IComparable {
+    public int X;
+    public int Y;
+    public char Tool; // in (T, C, N)
+    public int Duration = int.MaxValue;
+
+    public int CompareTo(object obj) {
+        // Assume other is a non-null Node
+        var other = (Node)obj;
+        int diff = this.X.CompareTo(other.X);
+        if (diff == 0) {
+            diff = this.Y.CompareTo(other.Y);
+            if (diff == 0) {
+                diff = this.Tool.CompareTo(other.Tool);
+
+                // ignore Duration
+            }
+        }
+        return diff;
+    }
+    public override bool Equals(object other) {
+        return this.CompareTo(other) == 0;
+    }
+    public override int GetHashCode() {
+        return this.X.GetHashCode() ^ this.Y.GetHashCode() ^ this.Tool.GetHashCode();
+    }
+}
+public class NodeDurationComparer : System.Collections.Generic.Comparer<Node> {
+    public override int Compare(Node objA, Node objB) {
+        // Assume both valid
+        int diff = ((Node)objA).Duration.CompareTo(((Node)objB).Duration);
+        if (diff == 0) {
+            diff = ((Node)objA).CompareTo((Node)objB);
+        }
+        return diff;
+    }
+}
+public class PriorityQueue
+{
+    private System.Collections.Generic.SortedSet<Node> queue = new System.Collections.Generic.SortedSet<Node>(new NodeDurationComparer());
+
+    public int Count { get { return queue.Count; } }
+   
+	public void Push(Node n) {
+		queue.Add(n);
+	}
+	public Node Pop() {
+		var front = queue.Min;
+		queue.Remove(front);
+		return front;
+	}
+	public void Remove(Node n) {
+		queue.Remove(n);
+	}
+}
+public class NodeHashSet
+{
+    private System.Collections.Generic.HashSet<Node> set = new System.Collections.Generic.HashSet<Node>();
+
+	public int Count { get { return set.Count; } }
+
+	public bool Add(Node n) {
+		return set.Add(n);
+	}   
+
+	public bool Contains(Node n) {
+		return set.Contains(n);
+	}   
+	public Node Get(Node n) {
+		Node o;
+		if (!set.TryGetValue(n, out o))
+			throw new System.Collections.Generic.KeyNotFoundException();
+		return o;
+	}   
+	public Node TryGet(Node n) {
+		Node o;
+		if (set.TryGetValue(n, out o)) 
+			return o;
+		return null;
+	}   
+}
+'
+
 function part2 {
     $depth, $targetX, $targetY = input
     $puzzle = [Puzzle]::new($depth, $targetX, $targetY)
-    $puzzle.Print($puzzle.TargetX + 3, $puzzle.TargetY + 3)
-    $puzzle.PrintTool('torch', $puzzle.TargetX + 3, $puzzle.TargetY + 3)
-    $puzzle.PrintTool('climbing gear', $puzzle.TargetX + 3, $puzzle.TargetY + 3)
-    $puzzle.PrintTool('neither', $puzzle.TargetX + 3, $puzzle.TargetY + 3)
 
-    $elapsed = 0#$puzzle.ComputeQuickestPath($puzzle.TargetX, $puzzle.TargetY)
+    # We're assuming the path never veers too far from a direct path
+    [int]$maxX = $puzzle.TargetX + 100
+    [int]$maxY = $puzzle.TargetY + 100
+    $null = $puzzle.ComputeRisk($maxX, $maxY)
 
-    Write-Host "Part 2: Fewest minutes to reach target is $elapsed."
+    [Node]$initialNode = @{x=0; y=0; tool='T'; duration=0}
+    [Node]$targetC = @{x=$puzzle.TargetX; y=$puzzle.TargetY; tool='C'}
+    [Node]$targetT = @{x=$puzzle.TargetX; y=$puzzle.TargetY; tool='T'}
+
+    [NodeHashSet]$nodes = [NodeHashSet]::new()
+    [NodeHashSet]$visited = [NodeHashSet]::new()
+    [PriorityQueue]$queue = [PriorityQueue]::new()
+
+    $null = $nodes.Add($initialNode)
+    $queue.Push($initialNode)
+
+    while ($queue.Count) {
+        # Select the node with the minimum duration (queue is sorted by duration)
+        [Node]$cur = $queue.Pop()
+        $null = $visited.Add($cur)
+
+        # break once we find the Torch solution. The Camping Gear solution takes an additional 7 seconds
+        # thus it may not be the shortest.
+        if ($cur -eq $targetT -or $cur -eq $targetC) {
+            break
+        }
+
+        $neighbors = @(@(($cur.X-1), $cur.Y), @(($cur.X+1), $cur.Y), @($cur.X, ($cur.Y+1)), @($cur.X, ($cur.Y-1)))
+        foreach($neighbor in $neighbors) {
+            [int]$x = $neighbor[0]
+            [int]$y = $neighbor[1]
+            if ($x -lt 0 -or $x -gt $maxX) {continue}
+            if ($y -lt 0 -or $y -gt $maxY) {continue}
+
+            # Rocky area (0) can use Torch or Climbing gear
+            # Wet area (1) can use Climbing gear or Neither
+            # Narrow area (2) can use Torch or Neither
+            [string]$allowedTools = @('TC', 'CN', 'TN')[$puzzle.getErosionLevel($x, $y) % 3]
+            foreach ($tool in $allowedTools.GetEnumerator()) {
+
+                [Node]$next = @{x=$x; y=$y; tool=$tool} 
+                if ($visited.Contains($next)) {
+                    continue
+                }
+
+                # 1 second to move
+                $next.Duration = $cur.Duration + 1
+
+                # 7 seconds to switch tools
+                if ($next.Tool -ne $cur.Tool) {
+                    $next.Duration += 7
+                }
+
+                # The Camping Gear solution always takes an additional
+                # 7 seconds to switch back to Torch
+                if ($next -eq $targetC) {
+                    $next.Duration += 7
+                }
+                
+                if (!$nodes.Add($next)) {
+                    # already exists
+                    [Node]$existing = $nodes.Get($next)
+                    if ($next.Duration -lt $existing.Duration) {
+                        # queue is sorted by duration, so remove before modifying. we'll reinsert it later.
+                        $null = $queue.Remove($existing)
+                        $existing.Duration = $next.Duration
+                    }
+                    $next = $existing
+                }
+
+                $queue.Push($next)
+            }
+        }
+    }
+
+    $minDuration = [int]::MaxValue
+    $targetT = $visited.TryGet($targetT)
+    if ($targetT -and $targetT.Duration -lt $minDuration) {
+        $minDuration = $targetT.Duration
+    }
+
+    $targetC = $visited.TryGet($targetC)
+    if ($targetC -and $targetC.Duration -lt $minDuration) {
+        $minDuration = $targetC.Duration
+    }
+    
+    Write-Host "Part 2: Fewest minutes to reach target is $minDuration."
 }
 
 function input {
     #      depth, targetX targetY
-    return   510,   10,      10
+    #return   510,   10,      10
     return 11394,    7,     701
 }
 #input
