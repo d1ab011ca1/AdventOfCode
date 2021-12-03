9ec80706fe4f6c0521ecef918cf1b566ebcc1360
John Belcher <jebelche@rockwellautomation.com>
2021-12-02 21:07:57 -0500

    2021 Day 2 using F#

---
+++ 2021/Day 2.fsx
@@ -0,0 +1,59 @@
+open System
+open System.IO
+
+let realInputText = File.ReadAllText("Day 2.txt")
+
+let sampleInputText =
+    """
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+    """
+
+let input =
+    let inputText = realInputText
+    //let inputText = sampleInputText
+
+    inputText.Trim().Split('\n')
+    |> Seq.map (fun s -> s.Trim())
+    |> Seq.where (not << String.IsNullOrEmpty)
+    |> Seq.map (fun s -> s.Split(' ', 2))
+    |> Seq.map (fun ss -> ss.[0], ss.[1] |> int)
+//input.Dump("input")
+
+let part1 () =
+    let (x, y) =
+        input
+        |> Seq.fold
+            (fun (x, y) ->
+                function
+                | ("up", c) -> x, y - c
+                | ("down", c) -> x, y + c
+                | (_, c) -> x + c, y)
+            (0, 0)
+
+    let pos = {| x = x; y = y |}
+    let answer = x * y
+    printfn "Part 1: %d, pos=%O\n" answer pos
+
+part1 ()
+
+let part2 () =
+    let (x, y, aim) =
+        input
+        |> Seq.fold
+            (fun (x, y, aim) ->
+                function
+                | ("up", c) -> x, y, aim - c
+                | ("down", c) -> x, y, aim + c
+                | (_, c) -> x + c, y + c * aim, aim)
+            (0, 0, 0)
+
+    let pos = {| x = x; y = y; aim = aim |}
+    let answer = x * y
+    printfn "Part 2: %d, pos=%O\n" answer pos
+
+part2 ()
