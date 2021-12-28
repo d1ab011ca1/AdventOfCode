5fa2329a8702e4ee0b01710a1d18e538760f82a7
John Belcher <jebelche@rockwellautomation.com>
2021-12-27 20:51:53 -0500

    Modify template to reference FSharpHelpers

--- template.fsx
+++ template.fsx
@@ -1,44 +1,39 @@
+// https://adventofcode.com/2021
+#if INTERACTIVE
+#load "FSharpHelpers.fsx"
+#r "nuget: MathNet.Numerics.FSharp" // https://numerics.mathdotnet.com/api/
+#r "nuget: FSharp.Collections.ParallelSeq"
+#endif
+
 open System
 open System.IO
+open FSharpHelpers
+// open MathNet.Numerics
+// open FSharp.Collections.ParallelSeq
 
-let digitToInt (c: char) =
-    match int c with
-    | n when (int '0') <= n && n <= (int '9') -> n - (int '0')
-    | _ -> failwithf "Invalid decimal digit: %A" c
-
-let hexDigitToInt (c: char) =
-    match int c with
-    | n when (int '0') <= n && n <= (int '9') -> n - (int '0')
-    | n when (int 'A') <= n && n <= (int 'F') -> n - (int 'A') + 10
-    | n when (int 'a') <= n && n <= (int 'f') -> n - (int 'a') + 10
-    | _ -> failwithf "Invalid hex digit: %A" c
-
-let (|DecChar|) = digitToInt
-let (|HexChar|) = hexDigitToInt
-
-let realInputText =
-    let inputPath = Path.ChangeExtension(fsi.CommandLineArgs.[0], ".txt")
-    File.ReadAllText(inputPath)
-
-let sampleInputText = """
-...
+let sampleInputText1 =
+    """
 """
 
-let inputs =
-    //let inputText = realInputText
-    let inputText = sampleInputText
+let inputText =
+    // File.ReadAllText(getInputFilePath ())
+    sampleInputText1
 
-    let lines =
-        inputText.Split('\n', StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+let parseInput (text: string) =
+    let lines: string[] = text |> parseInputText
+
+    // let charArrays = lines |> toCharArrays
+    // let wordArrays = lines |> toWordArrays
+    // let groups = lines |> toGroups "some group prefix"
 
     lines
-//printfn "%A" inputs
 
-let part1 () =
-    printfn "Part 1: "
+let inputs = inputText |> parseInput
+printfn "%A" inputs
 
-let part2 () =
-    printfn "Part 2: "
+let part1 () = printfn "Part 1: "
+
+let part2 () = printfn "Part 2: "
 
 part1 () //
 part2 () //
