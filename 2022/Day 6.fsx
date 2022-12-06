3dd8a6f3756c28bdca77910bdffba0cb0ab24b0c
John Belcher <4042025+foo-queue@users.noreply.github.com>
2022-12-06 01:24:38 -0500

    2022 Day 6

---
+++ 2022/Day 6.fsx
@@ -0,0 +1,42 @@
+// https://adventofcode.com/2022
+#if INTERACTIVE
+#load "../FSharpHelpers.fsx"
+#r "nuget: MathNet.Numerics.FSharp" // https://numerics.mathdotnet.com/api/
+#r "nuget: FSharp.Collections.ParallelSeq"
+#endif
+
+open System.IO
+open FSharpHelpers
+
+let sampleInputText1 =
+    """
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+"""
+
+let inputText = File.ReadAllText(getInputFilePath ())
+// let inputText = sampleInputText1
+
+let parseInput (text: string) =
+    text.Trim()
+
+let inputs = inputText |> parseInput
+printfn "%A" inputs
+
+let find len =
+        inputs 
+        |> Seq.windowed len
+        |> Seq.mapi (fun i w -> i+len, w)
+        |> Seq.where (fun (_, w) -> w |> Seq.distinct |> Seq.length = len)
+        |> Seq.head
+        |> fst
+
+let part1 () =
+    let n = find 4
+    printfn "Part 1: %A" n
+
+let part2 () =
+    let n = find 14
+    printfn "Part 2: %A" n
+
+part1 () // 1760
+part2 () // 2974
